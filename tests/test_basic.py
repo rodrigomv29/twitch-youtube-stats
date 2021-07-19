dc783333a18c5d4f8fd20b0094c5c410cc6ddc6b
Csantana1121 <christians1121@gmail.com>
2021-07-19 15:18:39 -0500

    workflow added

---
+++ tests/test_basic.py
@@ -0,0 +1,35 @@
+import unittest, sys
+
+sys.path.append('../WebTest') # imports python file from parent directory
+from Basicweb import app #imports flask app object
+
+class BasicTests(unittest.TestCase):
+
+    # executed prior to each test
+    def setUp(self):
+        self.app = app.test_client()
+
+    ###############
+    #### tests ####
+    ###############
+
+    def test_main_page(self):
+        response = self.app.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+        response = self.app.get('/home', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_second_page(self):
+        response = self.app.get('/second_page', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_page(self):
+        response = self.app.get('/about', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_register_page(self):
+        response = self.app.get('/register', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/test_users.py
@@ -0,0 +1,44 @@
+import unittest, sys, os
+
+sys.path.append('../WebTest')
+from Basicweb import app, db
+
+class UsersTests(unittest.TestCase):
+
+    # executed prior to each test
+    def setUp(self):
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+        self.app = app.test_client()
+        db.drop_all()
+        db.create_all()
+
+    ###############
+    #### tests ####
+    ###############
+
+    def register(self, username, email, password):
+        return self.app.post('/register',
+                            data=dict(username=username,
+                                      email=email,
+                                      password=password, 
+                                      confirm_password=password),
+                            follow_redirects=True)
+
+    def test_valid_user_registration(self):
+        response = self.register('test', 'test@example.com', 'FlaskIsAwesome')
+        self.assertEqual(response.status_code, 200)
+
+    def test_invalid_username_registration(self):
+        response = self.register('t', 'test@example.com', 'FlaskIsAwesome')
+        self.assertIn(b'Field must be between 2 and 20 characters long.', response.data)
+        response = self.register('Thisislongerthan20characters', 'test@example.com', 'FlaskIsAwesome')
+        self.assertIn(b'Field must be between 2 and 20 characters long.', response.data)
+
+    def test_invalid_email_registration(self):
+        response = self.register('test2', 'test@example', 'FlaskIsAwesome')
+        self.assertIn(b'Invalid email address.', response.data)
+        response = self.register('test2', 'testexample.com', 'FlaskIsAwesome')
+        self.assertIn(b'Invalid email address.', response.data)
+
+if __name__ == "__main__":
+    unittest.main()
